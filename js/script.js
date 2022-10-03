e0c067a48e269eaeee70c21cbbd158df822ef457
Fernanda Daniela Manzano <65313674+ManzanoFernanda@users.noreply.github.com>
2022-10-03 19:34:41 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+console.log("Ejecutando js.")
+var app = angular.module("app",[]);
+app.controller("appCtrl",function($scope, $rootScope){
+
+    console.log("Ejecutando Angular js");
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
